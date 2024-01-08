31db5ee75ee08e9485eec295c6c210ce6229fb07
Abir Stolov <abir.stolov@vaica.com>
2024-01-08 21:20:29 +0200

    reuse rabbitMQ connection

--- device-event-router/DeviceEventsRouter.cs
+++ device-event-router/DeviceEventsRouter.cs
@@ -9,7 +9,7 @@ public class DeviceEventsRouter(ILogger<DeviceEventsRouter> logger, RabbitMQSett
 {
     private readonly ILogger<DeviceEventsRouter> _log = logger;
     private readonly RabbitMQSettings _rabbitMQSettings = rabbitMQSettings;
-    // TODO Create a single open connection and reuse
+    private IConnection? rabbitMQConnection = null;
     public void RouteDeviceEvent(string received)
     {
         _log.LogDebug("RouteDeviceEvent was called with {received}", received);
@@ -20,12 +20,14 @@ public class DeviceEventsRouter(ILogger<DeviceEventsRouter> logger, RabbitMQSett
             HostName = _rabbitMQSettings.HostName,
             VirtualHost = _rabbitMQSettings.VirtualHost
         };
-        using IConnection conn = factory.CreateConnection();
-        using IModel channel = conn.CreateModel();
+        if (rabbitMQConnection is null || !rabbitMQConnection.IsOpen)
+            rabbitMQConnection = factory.CreateConnection();
+        using IModel channel = rabbitMQConnection.CreateModel();
         channel.QueueDeclare(queue: _rabbitMQSettings.Queue, durable: false, exclusive: false, autoDelete: false);
-        var message = "This is my first message";
+        var message = received;
         var encodedMessage = Encoding.UTF8.GetBytes(message);
         channel.BasicPublish("", _rabbitMQSettings.Queue, null, encodedMessage);
         _log.LogInformation($"Published message {message} to queue {_rabbitMQSettings.Queue}");
     }
+
 }
